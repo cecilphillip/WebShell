ab67e9ff4d661b258e3763324b518180f76056b6
Cecil Phillip <cecilphillip@gmail.com>
2014-05-21 17:44:48 -0400

    rename projects

--- NetBash/AssemblyLocator.cs
+++ NetBash/AssemblyLocator.cs
@@ -1,29 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Web;
-using System.Web.Compilation;
 
 namespace NetBash
 {
     internal static class AssemblyLocator
     {
-        private static ReadOnlyCollection<Assembly> AllAssemblies = null;
-        private static ReadOnlyCollection<Assembly> BinFolderAssemblies = null;
+        private static ReadOnlyCollection<Assembly> allAssemblies;
 
         public static ReadOnlyCollection<Assembly> GetAssemblies()
         {
-            if(AllAssemblies != null) return AllAssemblies;
+            if(allAssemblies != null) return allAssemblies;
             //TODO: remove this after OWIN implementaiton new ReadOnlyCollection<Assembly>(BuildManager.GetReferencedAssemblies().Cast<Assembly>().ToList());
-            AllAssemblies = new ReadOnlyCollection<Assembly>((from file in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + @"bin\")
+            allAssemblies = new ReadOnlyCollection<Assembly>((from file in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + @"bin\")
                                                   where Path.GetExtension(file) == ".dll"
                                                   select Assembly.LoadFrom(file)).ToList());
 
 
-            return AllAssemblies;
+            return allAssemblies;
         }
     }
 }
